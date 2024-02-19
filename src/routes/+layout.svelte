3f408372dcc4b514f814005d601dbb29dabc8359
Jarrod Davis <developer@jarrodldavis.com>
2024-02-19 01:38:49 +0000

    inject vercel analytics and speed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
+	import { inject as injectAnalytics } from '@vercel/analytics';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 	import type { Snippet } from 'svelte';
 	import './global.css';
+
+	injectAnalytics();
+	injectSpeedInsights();
 
 	const { children } = $props<{ children: Snippet }>();
 </script>
