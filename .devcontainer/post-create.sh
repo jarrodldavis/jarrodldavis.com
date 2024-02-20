fc4c9b197cd1fee0402782d400eea72f88936bd8
Jarrod Davis <developer@jarrodldavis.com>
2024-02-20 01:07:07 +0000

    update dependencies

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,11 +2,11 @@
 
 set -e
 
+echo '==> Installing Playwright browsers...'
+pnpm playwright install --with-deps
+
 echo '==> Performing initial Vite build...'
 pnpm generate
 pnpm build
-
-echo '==> Installing Playwright browsers...'
-pnpm playwright install --with-deps
 
 echo '==> Done!'
