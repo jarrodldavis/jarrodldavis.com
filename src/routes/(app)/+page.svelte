1aadad38e88284943e7bffdcdcc34918a880bed3
Jarrod Davis <developer@jarrodldavis.com>
2024-03-15 19:39:58 +0000

    lift layout markup to layout component

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -2,10 +2,8 @@
 	import About from '$lib/About.svelte';
 	import Education from '$lib/Education.svelte';
 	import Experience from '$lib/Experience.svelte';
-	import HeaderContents from '$lib/HeaderContents.svelte';
 	import Section from '$lib/Section.svelte';
 	import { set_skills } from '$lib/SkillUsage.svelte';
-	import { set_heading_level } from '$lib/heading';
 	import type { PageData } from './$types';
 
 	interface Props {
@@ -14,62 +12,18 @@
 
 	const { data }: Props = $props();
 	set_skills(data.skills);
-	set_heading_level(1);
 </script>
 
-<main>
-	<header>
-		<HeaderContents />
-	</header>
+<About list_entries={data.intro} />
 
-	<article>
-		<About list_entries={data.intro} />
+<Section title="Experience">
+	{#each data.experience as entry, index (index)}
+		<Experience {entry} />
+	{/each}
+</Section>
 
-		<Section title="Experience">
-			{#each data.experience as entry, index (index)}
-				<Experience {entry} />
-			{/each}
-		</Section>
-
-		<Section title="Education">
-			{#each data.education as entry, index (index)}
-				<Education {entry} />
-			{/each}
-		</Section>
-	</article>
-</main>
-
-<svelte:head>
-	<!-- var(--surface-2) -->
-	<meta name="theme-color" content="rgb(233, 236, 239)" media="(prefers-color-scheme: light)" />
-	<meta name="theme-color" content="rgb(52, 58, 64)" media="(prefers-color-scheme: dark)" />
-</svelte:head>
-
-<style>
-	:root {
-		--safe-area-inset-block-start: max(env(safe-area-inset-top, 0), var(--size-fluid-3));
-		--safe-area-inset-block-end: max(env(safe-area-inset-bottom, 0), var(--size-fluid-3));
-		--safe-area-inset-inline-start: max(env(safe-area-inset-left, 0), var(--size-fluid-3));
-		--safe-area-inset-inline-end: max(env(safe-area-inset-right, 0), var(--size-fluid-3));
-	}
-
-	header {
-		padding-block: var(--safe-area-inset-block-start) var(--size-fluid-3);
-		padding-inline: var(--safe-area-inset-inline-start) var(--safe-area-inset-inline-end);
-		background-color: var(--surface-2);
-		box-shadow: var(--shadow-3);
-	}
-
-	article {
-		padding-block: var(--safe-area-inset-block-start) var(--safe-area-inset-block-end);
-		padding-inline: var(--safe-area-inset-inline-start) var(--safe-area-inset-inline-end);
-		box-sizing: content-box;
-		font-size: var(--font-size-2);
-		max-inline-size: calc(var(--size-content-3));
-		margin: 0 auto;
-	}
-
-	article :global(li) {
-		margin-block-end: var(--size-relative-1);
-	}
-</style>
+<Section title="Education">
+	{#each data.education as entry, index (index)}
+		<Education {entry} />
+	{/each}
+</Section>
