d01f600cf7d33befa6f4dbead706d906e11a5c8e
Jarrod Davis <developer@jarrodldavis.com>
2024-07-19 03:02:39 +0000

    update twitter image

--- app/twitter-image.tsx
+++ app/twitter-image.tsx
@@ -4,8 +4,21 @@ import { ImageResponse } from "next/og";
 
 export const size = { width: photo.width, height: photo.height };
 
-export const alt = "A Memoji of me on a green background.";
+export const alt = "A Memoji of me in a green circle.";
 
 export default async function Image() {
-  return new ImageResponse(<SatoriImage src={photoContents} />, { ...size });
+  return new ImageResponse(
+    (
+      <div tw="flex h-full w-full bg-zinc-900 p-8">
+        <SatoriImage
+          src={photoContents}
+          style={{
+            boxShadow: "inset 0 4px 12px 0 #09090b",
+          }}
+          tw="rounded-full border-4 border-[#09090b] bg-zinc-700 p-6"
+        />
+      </div>
+    ),
+    { ...size },
+  );
 }
