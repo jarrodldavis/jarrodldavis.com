a2ed670e5c13b6c44900c3c27485093ddcb0f6c9
Jarrod Davis <developer@jarrodldavis.com>
2024-07-22 04:20:31 +0000

    sentry cleanup

--- app/spotlight/contextlines/route.ts
+++ app/spotlight/contextlines/route.ts
@@ -1,3 +1,5 @@
+export const runtime = "edge";
+
 // https://github.com/getsentry/spotlight/issues/275
 // https://github.com/getsentry/spotlight/blob/a8c09cd8629677ab3eed4bf7000de4c7068538ee/packages/overlay/src/integrations/sentry/sentry-integration.ts#L63-L71
 export function PUT() {
