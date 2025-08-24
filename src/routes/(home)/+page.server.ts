b11117830c99926d38ce9af0f12b1a3b54f80314
Jarrod Davis <developer@jarrodldavis.com>
2025-08-24 07:12:06 -0600

    use production meta url

--- src/routes/(home)/+page.server.ts
+++ src/routes/(home)/+page.server.ts
@@ -1,5 +1,5 @@
 import { building } from '$app/environment';
-import { VERCEL_URL } from '$env/static/private';
+import { VERCEL_ENV, VERCEL_PROJECT_PRODUCTION_URL, VERCEL_URL } from '$env/static/private';
 import { load_resume } from '$lib/server';
 import { fetch_image_info } from '$lib/server/image';
 import type { PageServerLoad } from './$types';
@@ -8,7 +8,7 @@ export const prerender = true;
 
 export const load: PageServerLoad = async ({ url }) => {
 	return {
-		base_url: building ? `https://${VERCEL_URL}` : url.toString(),
+		base_url: get_base_url(url),
 		resume: load_resume(),
 		images: {
 			opengraph: await fetch_image_info('opengraph-image.png'),
@@ -19,3 +19,13 @@ export const load: PageServerLoad = async ({ url }) => {
 		}
 	};
 };
+
+function get_base_url(load_url: URL) {
+	if (!building) {
+		return load_url.toString();
+	} else if (VERCEL_ENV === 'production') {
+		return `https://${VERCEL_PROJECT_PRODUCTION_URL}`;
+	} else {
+		return `https://${VERCEL_URL}`;
+	}
+}
