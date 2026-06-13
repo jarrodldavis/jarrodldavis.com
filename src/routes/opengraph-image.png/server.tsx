4e52e1bcf10d0ce875f4570fcd53e3a7a526c406
Jarrod Davis <developer@jarrodldavis.com>
2026-06-12 18:17:00 -0600

    update opengraph font

--- src/routes/opengraph-image.png/server.tsx
+++ src/routes/opengraph-image.png/server.tsx
@@ -1,7 +1,7 @@
 import { read } from '$app/server';
 import SatoriImg, { create_handler, type ImageComponentProps } from '$lib/satori-img';
-import PlexMonoBold from '@ibm/plex-mono/fonts/complete/woff/IBMPlexMono-Bold.woff';
-import PlexMonoSemiBold from '@ibm/plex-mono/fonts/complete/woff/IBMPlexMono-SemiBold.woff';
+import SourceCodeBold from 'source-code-pro/OTF/SourceCodePro-Bold.otf';
+import SourceCodeSemiBold from 'source-code-pro/OTF/SourceCodePro-SemiBold.otf';
 import type { RequestHandler } from './$types';
 
 const OpenGraphImage = ({ profile, src }: ImageComponentProps) => (
@@ -28,8 +28,8 @@ const OpenGraphImage = ({ profile, src }: ImageComponentProps) => (
 const width = 1200;
 const height = 630;
 const fonts = [
-	{ data: await read(PlexMonoBold).arrayBuffer(), name: 'IBM Plex Mono', weight: 700 } as const,
-	{ data: await read(PlexMonoSemiBold).arrayBuffer(), name: 'IBM Plex Mono', weight: 600 } as const
+	{ data: await read(SourceCodeBold).arrayBuffer(), name: 'Source Code', weight: 700 } as const,
+	{ data: await read(SourceCodeSemiBold).arrayBuffer(), name: 'Source Code', weight: 600 } as const
 ];
 
 export const GET: RequestHandler = create_handler(OpenGraphImage, { width, height, fonts });
