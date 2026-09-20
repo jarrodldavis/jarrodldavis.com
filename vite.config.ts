35603677031ee900faf85bdcdd70ffddc5cb129a
Jarrod Davis <developer@jarrodldavis.com>
2026-09-19 18:37:09 -0600

    update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
-import prepare from './prepare/vite-plugin';
+import prepare from './prepare/vite-plugin.ts';
 
 export default defineConfig({
 	plugins: [prepare(), tailwindcss(), enhancedImages(), sveltekit()],
